5074fadf62221ef599a8cc3ecc78f702e800143a
parsa74n <parsa74n@gmail.com>
2022-07-22 11:01:03 +0430

    uploading project

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using SignalRCore.Chat.Mvc.Data;
+using SignalRCore.Chat.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCore.Chat.Mvc.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly UserManager<ChatUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public ChatHub(ApplicationDbContext context, UserManager<ChatUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task BroadcastFromClient(string message)
+        {
+            try
+            {
+                var currentUser = await _userManager.GetUserAsync(Context.User);
+                String userEmailFirstSection = currentUser.Email.Split('@').First();
+                Message m = new Message()
+                {
+                    MessageBody = message,
+                    MessageDtTm = DateTime.Now,
+                    FromUser = currentUser
+                };
+
+                _context.Messages.Add(m);
+                await _context.SaveChangesAsync();
+
+                await Clients.All.SendAsync(
+                    "Broadcast",
+                    new
+                    {
+
+                        messageBody = m.MessageBody,
+                        fromUser = userEmailFirstSection,
+                        messageDtTm = m.MessageDtTm.ToString(
+                                "hh:mm tt MMM dd", CultureInfo.InvariantCulture
+                            )
+                    });
+            }
+            catch (Exception ex)
+            {
+                await Clients.Caller.SendAsync("HubError", new { error = ex.Message });
+            }
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var currentUser = await _userManager.GetUserAsync(Context.User);
+            String userEmailFirstSection = currentUser.Email.Split('@').First();
+            await Clients.All.SendAsync(
+                "UserConnected",
+                new
+                {
+                    userId = userEmailFirstSection,
+                    connectionId = Context.ConnectionId,
+                    connectionDtTm = DateTime.Now,
+                    messageDtTm = DateTime.Now.ToString(
+                                "hh:mm tt MMM dd", CultureInfo.InvariantCulture
+                            )
+                }) ;
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            var currentUser = await _userManager.GetUserAsync(Context.User);
+            String userEmailFirstSection = currentUser.Email.Split('@').First();
+
+            await Clients.All.SendAsync("UserDisconnected",
+                new { 
+                elementId=userEmailFirstSection,
+                message=$"User disconnected, user id: {userEmailFirstSection}",
+                    messageDtTm = DateTime.Now.ToString(
+                                "hh:mm tt MMM dd", CultureInfo.InvariantCulture
+                            )
+                }
+                );
+        }
+    }
+}
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,72 @@
+﻿var connection = null;
+
+$(function () {
+    if (connection === null) {
+        connection = new signalR.HubConnectionBuilder()
+            .withUrl("/chatHub")
+            .build();
+
+        connection.on("Broadcast", function (response) {
+            $("#chatRoom").append(
+              
+                '<div class="media w-50 ml-auto mb-3">' +
+                '<img src="https://img.icons8.com/officel/16/000000/user.png" alt="user" width="50" class="rounded-circle">' +
+                '<div class="media-body">' +
+                '<div class="bg-primary rounded py-2 px-3 mb-2">' +
+                '<p class="text-small mb-0 text-white">' + response.messageBody + '</p>' +
+                '</div>' +
+                '<p class="small text-muted">' + response.fromUser + '</p>' +
+                '<p class="small text-muted">' + response.messageDtTm + '</p>' +
+                '</div>' +
+                '</div>'
+            );
+
+            $('#chatRoom').stop().animate({
+                scrollTop: $('#chatRoom')[0].scrollHeight
+            });
+        });
+
+        connection.on("UserConnected", function (response) {
+    
+            $("#chatRoom").append(
+                '<div class="media w-50 mb-3">' +
+                '<img src="https://img.icons8.com/dusk/64/000000/user-female-skin-type-1-2.png" alt="user" width="50" class="rounded-circle">' +
+                '<div class="media-body ml-3">' +
+                '<div class="bg-light rounded py-2 px-3 mb-2">' +
+                '<p class="text-small mb-0 text-muted">به چت روم خوش آمدید! ' + response.userId+'</p>' +
+                '</div>' +
+                '<p class="small text-muted">' + response.messageDtTm + '</p>' +
+                '</div>'
+            );
+        });
+
+        connection.on("UserDisconnected", function (response) {
+            $("#chatRoom").append(
+                '<div class="media w-50 mb-3">' +
+                '<img src="https://img.icons8.com/dusk/64/000000/user-female-skin-type-1-2.png" alt="user" width="50" class="rounded-circle">' +
+                '<div class="media-body ml-3">' +
+                '<div class="bg-light rounded py-2 px-3 mb-2">' +
+                '<p class="text-small mb-0 text-muted">از چت خارج شد! ' + response.elementId + '</p>' +
+                '</div>' +
+                '<p class="small text-muted">' + response.messageDtTm + '</p>' +
+                '</div>'
+            );
+            alert(response.message);
+           
+        });
+
+        connection.on("HubError", function (response) {
+            alert(response.error);
+        });
+
+        connection.start().then(function () {
+            document.getElementById('sendButton').onclick = function () {
+                var message = document.getElementById("messageInput").value;
+                connection.invoke("BroadcastFromClient", message)
+                    .catch(function (err) {
+                        return console.error(err.toString());
+                    });
+            };
+        });
+    }
+});
